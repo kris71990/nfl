8a88dc8190906851b0cee4659a67a9ebad0c8ba1
Kris Sakarias <ksakarias@gmail.com>
2021-09-15 17:10:08 -0700

    edit abbreviations to support score entry, write_scores function added to record weekly scores

--- nflscores.py
+++ nflscores.py
@@ -1,7 +1,7 @@
 # nflscores.py 
 # gets nfl weekly scores and enters them into spreadsheet 
 
-import requests, bs4, openpyxl, os, sys, re, nflteams, byeteams, teaminfo, weekInfo
+import requests, bs4, openpyxl, os, sys, re, teaminfo, weekInfo
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -22,9 +22,58 @@ def get_scores(week):
   for each in scores_raw:
     score = each.getText()
     score_split = score.split(' ')
-    scores[score_split[0]] = '%s-%s' % (score_split[1].strip(','), score_split[3])
+    if (score_split[1].strip(', ') == score_split[3]):
+      scores[score_split[0] + '-' + scores[score_split[2]]] = '%s-%s' % (score_split[1].strip(','), score_split[3])
+    else:
+      scores[score_split[0]] = '%s-%s' % (score_split[1].strip(','), score_split[3])
 
   print(scores)
   return scores
 
-get_scores(week_num)
+def write_scores(week):
+  print('\nWriting to spreadsheet...\n')
+
+  scores = get_scores(week)
+  print(scores)
+
+  os.chdir(os.getenv('DESKTOP'))
+  wb = openpyxl.load_workbook(os.getenv('EXCEL_FILE'))
+  sheet = wb.get_sheet_by_name('Sheet 1')
+
+  # find spreadsheet start row and write scores to appropriate cells
+  write_score_index = 0
+  start_row = 0
+  if 'Week' not in week_num:
+    if (int(week_num) > 17):
+      start_week = weekInfo.playoff_week_titles[week_num]
+    else:
+      start_week = 'Week ' + week_num
+  else:
+    start_week = week_num
+
+  for cell in sheet.columns[0]:
+    if cell.value == start_week:
+      start_row += 2
+      for row_num in range(start_row, len(scores) + start_row):
+        score_cell = sheet.cell(row=row_num, column=2)
+        matchup_cell = sheet.cell(row=row_num, column=1)
+        matchup_cell_text = matchup_cell.value
+
+        rx = re.compile(r'([a-zA-Z\s.]+)', re.I)
+        teams_raw = rx.findall(matchup_cell_text)
+        teams = [x.strip(' ') for x in teams_raw]
+        team1abbr = teaminfo.abbreviations[teams[0]].upper()
+        team2abbr = teaminfo.abbreviations[teams[1][3:]].upper()
+        
+        if (team1abbr in scores):
+          score_cell.value = '%s %s' % (team1abbr, scores[team1abbr])
+        elif (team2abbr in scores):
+          score_cell.value = '%s %s' % (team2abbr, scores[team2abbr])
+        write_score_index += 1       
+    else: 
+      start_row += 1
+
+  print('Done')
+  wb.save(os.getenv('EXCEL_FILE_NEW'))
+
+write_scores(week_num)
