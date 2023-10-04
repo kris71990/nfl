292c073b144ca13cc0601b547bff235eebabbfdc
Zack Shainsky <zshainsky@gmail.com>
2023-10-04 08:45:06 -0700

    Add user agent header to get_scores_soup

--- assets/soup.py
+++ assets/soup.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 # get scores soup per week
 def get_scores_soup(week, season_type):
   url = 'http://www.espn.com/nfl/schedule/_/week/' + week + '/seasontype/' + season_type
+  # ESPN requires a User-Agent header in the request
+  hdr = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'}
   res = requests.get(url)
   res.raise_for_status()
   soup = BeautifulSoup(res.text, 'html.parser')
